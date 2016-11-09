bc67fc718434a90c445bab08bbcecd04c901f795
wangchaogang <765483472@qq.com>
2016-11-09 17:08:12 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 body.style.height = window.innerHeight+'px'
 
 names.onclick= function(){
-	text.innerHTML = "姓名：王超岗<br>性别：男<br>年龄：20<br>职业：web前端工程师<br>QQ：765483472"
+	text.innerHTML = "姓名：王超岗<br>性别：男<br>年龄：20<br>职业：web前端工程师<br>QQ：765483472<br> 性格：活泼开朗，乐于助人。 <br>自我介绍：喜欢唱歌、画画、并且有很高的天赋，活泼开朗，无不良嗜好，有很高的社会适应能力"
 }
